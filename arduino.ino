9ee102fa50eb91e43257b4400a883eb76661e90d
Panos Tsakmakis <panos_dabest@yahoo.gr>
2016-03-25 13:18:30 +0200

    add variable for the tank data

--- arduino.ino
+++ arduino.ino
@@ -6,11 +6,13 @@
 byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01 }; // RESERVED MAC ADDRESS
 EthernetClient client;
 
+const float pi = 3.14;
 
 #define TRIG 10      // Trigger pin
 #define ECHO 13      // Echo pin
 #define DHTPIN 2 // SENSOR PIN
 #define DHTTYPE DHT11 // SENSOR TYPE - THE ADAFRUIT LIBRARY OFFERS SUPPORT FOR MORE MODELS
+
 DHT dht(DHTPIN, DHTTYPE);
 
 long previousMillis = 0;
@@ -19,9 +21,14 @@ long interval = 250000; // READING INTERVAL
 
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
-int t = 0;  // TEMPERATURE VAR
-int h = 0;  // HUMIDITY VAR
+int temprature = 0;  // TEMPERATURE VAR
+int humidity = 0;  // HUMIDITY VAR
+int tank_liters = 0;  // HUMIDITY VAR
 String data;
+
+int height = 2000;
+int liters = 0;
+int radius = 50;
 
 void setup() { 
   Serial.begin(115200);
@@ -33,8 +40,8 @@ void setup() {
   dht.begin(); 
   delay(10000); // GIVE THE SENSOR SOME TIME TO START
 
-  h = (int) dht.readHumidity(); 
-  t = (int) dht.readTemperature(); 
+  humidity = (int) dht.readHumidity(); 
+  temprature = (int) dht.readTemperature(); 
 
   data = "";
 }
@@ -44,11 +51,11 @@ void loop(){
   currentMillis = millis();
   if(currentMillis - previousMillis > interval) { // READ ONLY ONCE PER INTERVAL
     previousMillis = currentMillis;
-    h = (int) dht.readHumidity();
-    t = (int) dht.readTemperature();
+    humidity = (int) dht.readHumidity();
+    temprature = (int) dht.readTemperature();
   }
 
-  data = "temp1=" + t + "&hum1=" + h;
+  data = "temp1=" + temprature + "&hum1=" + temprature + "&lit1=" + tank_liters;
 
   if (client.connect("www.*****.*************.com",80)) { // REPLACE WITH YOUR SERVER ADDRESS
     client.println("POST /add.php HTTP/1.1"); 
@@ -67,5 +74,8 @@ void loop(){
   delay(300000); // WAIT FIVE MINUTES BEFORE SENDING AGAIN
 }
 
+int tank_calculator(){
+
+}
 
 
